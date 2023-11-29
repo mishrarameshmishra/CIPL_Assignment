a8e4109f541fd948b8df940bff09279c03298896
Ramesh Mishra <mishra.rameshmishra@gmail.com>
2023-11-29 17:56:33 +0530

    CIPL Assignment

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using CIPL_Assignment.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+
 
 namespace CIPL_Assignment.Controllers
 {
@@ -13,11 +17,60 @@ namespace CIPL_Assignment.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string url = "https://dghindia.gov.in/index.php/all_story?type=1&page=notice";
+            string htmlContent = await GetHtmlContentAsync(url);
+            var ulData = ParseHtml(htmlContent);
+            return View(ulData);
         }
+        private async Task<string> GetHtmlContentAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+        private List<NoticeModel> ParseHtml(string htmlContent)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlContent);
 
+            var newsItems = new List<NoticeModel>();
+            var ulNode = htmlDocument.DocumentNode.SelectSingleNode("//div[contains(@style, 'padding:20px;')]/ul");
+
+            if (ulNode != null)
+            {
+                var liNodes = ulNode.SelectNodes(".//li");
+
+                if (liNodes != null)
+                {
+                    foreach (var liNode in liNodes)
+                    {
+                        var aNode = liNode.SelectSingleNode(".//a");
+                        if (aNode != null)
+                        {
+                            var href = aNode.GetAttributeValue("href", "");
+                            var title = aNode.InnerText.Trim();
+                            if (href.Contains("dghindia.gov.in"))
+                            {
+                                var newsItem = new NoticeModel { Url = href, Title = title, UrlType = "Internal" };
+                                newsItems.Add(newsItem);
+                            }
+                            else
+                            {
+                                var newsItem = new NoticeModel { Url = href, Title = title, UrlType = "External" };
+                                newsItems.Add(newsItem);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return newsItems;
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -29,4 +82,5 @@ namespace CIPL_Assignment.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
 }
---
+++ Models/NoticeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CIPL_Assignment.Models
+{
+    public class NoticeModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string UrlType { get; set; }
+    }
+}
